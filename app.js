184f191e9b719ccd13d82eb9c86e7a8550360a90
mard2499 <mige34.99@gmail.com>
2022-02-22 19:24:40 -0500

    sup

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 
     console.log("Servidor corriendo en el puerto 3000");
 });
